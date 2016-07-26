b0c6bdcf37796dbfc341a45f475389e8f4653560
Paul Gower <pgower@stmonicas.qld.edu.au>
2016-07-26 15:40:54 +1000

    Removed the _with_http_info type methods, it seems that information is always useful. api_client now tries to extract the pagination information and add it to the response responses are now returned as a dictionary

--- test.py
+++ test.py
@@ -8,5 +8,9 @@ from apis.users import UsersApi
 
 u = UsersApi()
 
-r = u.list_users_in_account(account_id=1)
+r = u.list_users_in_account(account_id=1, per_page=50, page=20)
+print r['data']
+print len(r['data'])
+r = u.list_user_page_views(2)
+print r['data']
 import pdb;pdb.set_trace()
